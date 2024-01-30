4a118510c8697cf5595013801a0e16b8eba81087
EldinMrkonjic <142885255+EldinMrkonjic@users.noreply.github.com>
2024-01-30 09:39:25 +0100

    Add files via upload

---
+++ ArrayÜbung/ArrayÜbung/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ArrayÜbung
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] names = new string[5];
+
+           
+            int[] numbers = new int[5];
+
+           
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Enter name {i + 1}: ");
+                names[i] = Console.ReadLine();
+
+                Console.Write($"Enter number {i + 1}: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+           
+            Console.WriteLine("\nEntered names and numbers:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Name {i + 1}: {names[i]}, Number {i + 1}: {numbers[i]}");
+            }
+        }
+    }
+}
